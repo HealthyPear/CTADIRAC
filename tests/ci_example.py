a06832091c2b9339f2abea92c4736d6a3de27ab6
Johan Bregeon <bregeon@in2p3.fr>
2020-12-10 15:04:20 +0100

    add dumb CI for gitlab

---
+++ tests/ci_example.py
@@ -0,0 +1,9 @@
+""" Test gitlab CI
+
+  December 10th 2020 - J. Bregeon
+                  bregeon@in2p3.fr
+"""
+
+from CTADIRAC.Core.Utilities import tool_box
+
+print(tool_box.DATA_LEVEL_METADATA_ID)
